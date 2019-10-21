0f79ce7d85e478e1f5c5e0e6616e85911ba72301
Rafael Faria Carvalho <rafael.carvalho@programmers.com.br>
2019-10-21 16:45:53 -0300

    Delete product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,140 @@
-import { Component } from '@angular/core';
+import { ProductsService } from "./products.service";
+import { Component } from "@angular/core";
+import { Observer, Observable } from "rxjs";
+import { Product } from "./product.model";
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+import { MaterialModule } from './material.module';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'node-udemy-client-http';
+  simpleReqProductObs$: Observable<Product[]>;
+  productsErrorHandling: Product[];
+  productsLoading: Product[];
+  isLoading: boolean = false;
+  productsIds: Product[];
+  newlyProducts: Product[] = [];
+  productsToDelete: Product[] = [];
+
+  constructor(private productsService: ProductsService,
+    private snackBar: MatSnackBar) {}
+
+  ngOnInit() {}
+
+  getSimpleHttpRequest() {
+    this.simpleReqProductObs$ = this.productsService.getProdutos();
+  }
+  getProductsWithErrorHandling() {
+    this.productsService.getProdutosError()
+    .subscribe(
+      (prods) => {
+        this.productsErrorHandling = prods; },
+      (err) => {
+        console.log(err);
+        console.log('Message: ' + err.error.msg);
+        console.log('Status code: ' + err.status);
+        const config = new MatSnackBarConfig();
+        config.duration = 2000;
+        config.panelClass = ['snack_err']; // snack_err é a classe no style.css
+        if (err.status === 0) { // Quando está offline o status code é 0.
+          this.snackBar.open('Could not connect to the server', '', config);
+        }
+        else {
+          this.snackBar.open(err.error.msg, '', config);
+        }
+      }
+    );
+  }
+  getProductsWithErrorHandlingOK() {
+    this.productsService.getProdutosDelay()
+    .subscribe(
+      (prods) => {
+        this.productsErrorHandling = prods;
+        let config = new MatSnackBarConfig();
+        config.duration = 2000;
+        config.panelClass = ['snack_ok']; // snack_ok é a classe no style.css
+        this.snackBar.open('Products Successefuly loaded!', '', config);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  getProductsLoading(){
+    this.isLoading = true;
+    this.productsService.getProdutosDelay()
+    .subscribe(
+      (prods) => {
+        this.productsLoading = prods;
+        this.isLoading = false;
+      },
+      (err) => {
+        console.log(err);
+        this.isLoading = false;
+      }
+    );
+  }
+
+  getProductsIds() {
+    this.productsService.getProductIds()
+      .subscribe((ids) => {
+        this.productsIds = ids.map(id => ({_id: id, name: '', department: '', price: 0}));
+      });
+  }
+
+  loadName(id: string) {
+    this.productsService.getProductName(id)
+      .subscribe( name => {
+        let index = this.productsIds.findIndex(p => p._id === id);
+        if (index >= 0){
+          this.productsIds[index].name = name;
+        }
+      }
+
+      );
+  }
+
+  saveProduct(name: string, department: string, price: number) {
+    let p = {name, department, price};
+    this.productsService.saveProduct(p)
+      .subscribe(
+        (p:Product) => {
+          console.log(p);
+          this.newlyProducts.push(p);
+        },
+        (err) => {
+          console.log(err);
+          let config = new MatSnackBarConfig();
+          config.duration = 2000;
+          config.panelClass = ['snack_err'];
+          if (err.status === 0)
+            this.snackBar.open('Could not connect to the server','',config);
+          else
+            this.snackBar.open(err.error.msg,'',config);
+        }
+      );
+  }
+
+  loadProductsToDelete() {
+    this.productsService.getProdutos()
+      .subscribe((prods) => this.productsToDelete = prods);
+  }
+
+  deleteProduct(p:Product){
+    this.productsService.deleteProduct(p)
+      .subscribe(
+        (res) =>{
+          let i = this.productsToDelete.findIndex(prod => p._id === prod._id);
+          if (i>=0)
+            this.productsToDelete.splice(i,1);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { ProductsService } from './products.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
